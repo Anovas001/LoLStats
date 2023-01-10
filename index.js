d67e35672d8684199bd67eb09c96fa2c59e216c7
Novas <aniolnovas@gmail.com>
2023-01-10 22:15:30 +0100

    Commit

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const cv = require('opencv4nodejs');
+const gm = require('gm');
 
-const mat = cv.imread('./test1.png');
-const threshold = mat.threshold(0, 255, cv.THRESH_BINARY);
+gm('test1.png')
+    .blackThreshold(100 ,100 , 100 , 0.5)
+    .write("threshold1.png", function (err) {
+        if (!err) console.log('done');
+        else console.log(err)
+    });
 
-const ocr = new cv.OCR();
-const text = ocr.run(threshold);
-console.log(text);
+const tesseract = require("node-tesseract-ocr")
+
+const config = {
+    lang: "eng",
+    oem: 1,
+    psm: 3,
+}
+
+tesseract
+    .recognize("threshold1.png", config)
+    .then((text) => {
+        console.log("Result:", text)
+    })
+    .catch((error) => {
+        console.log(error.message)
+    })
